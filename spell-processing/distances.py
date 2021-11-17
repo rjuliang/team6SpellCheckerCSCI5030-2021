d058f31c1a3bc03ee0dff1f5212e0e1e4ac9da17
JULIAN_R <julian.ramirez55@gmail.com>
2021-11-16 18:05:50 -0600

    adding copyright recognition of some functions

--- spell-processing/distances.py
+++ spell-processing/distances.py
@@ -1,3 +1,13 @@
+
+"""
+Special thanks to Peter Norvig for providing this amazing solution.
+
+Spelling Corrector in Python 3; see http://norvig.com/spell-correct.html
+
+Copyright (c) 2007-2016 Peter Norvig
+MIT license: www.opensource.org/licenses/mit-license.php
+"""
+
 def edits1(word):
     "All edits that are one edit away from `word`."
     letters    = 'áéíóúabcdefghijklmnopqrstuvwxyz'
